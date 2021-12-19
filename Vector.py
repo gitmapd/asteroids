31c7315cbcf6061d1cf0ffb16f18a6396e76654c
gitmapd <50059062+gitmapd@users.noreply.github.com>
2021-12-19 01:04:18 +0000

    Add files via upload

--- Vector.py
+++ Vector.py
@@ -27,10 +27,16 @@ class Vector:
             return Vector(self.x/other,self.y/other, self.z/other)
         else:
             raise TypeError("operand must int or float")
+    def mul_elems(self,other):
+        """return a new vector holding each element multipled , = "hadamard product", not the same as scaling or dot product""" 
+        if isinstance(other,Vector):
+            return (self.x*other.x , self.y*other.y , self.z*other.z)
+    def scaling_factor(self,scaling_fac):
+        if isinstance(scaling_fac,(int,float)):
+            return Vector(self.x*scaling_fac,self.y*scaling_fac,self.z*scaling_fac)  
 
 
 a=Vector(2,3,4)
 b=Vector(2,3,4)
-d=3
-c=a/d
+c=b.mul_elems(a)
 print(c)
